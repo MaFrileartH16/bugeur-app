fa7af2d5e6364aa771056027a56b67a82f70ad5f
MaFrileartH16 <muhammadfauzulhanif16@gmail.com>
2025-01-11 15:40:19 +0700

    feat(model): create Project model

---
+++ app/Models/Project.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Project extends Model
+{
+  use HasFactory;
+
+  public $incrementing = false;
+  protected $primaryKey = 'project_id';
+  protected $keyType = 'string';
+
+  protected $fillable = [
+    'project_id',
+    'manager_id',
+    'title',
+  ];
+
+  public function manager(): BelongsTo
+  {
+    return $this->belongsTo(User::class, 'manager_id');
+  }
+
+  public function bugs(): HasMany
+  {
+    return $this->hasMany(Bug::class, 'project_id');
+  }
+
+  public function members(): BelongsToMany
+  {
+    return $this->belongsToMany(User::class, 'working_on', 'project_id', 'user_id');
+  }
+}
