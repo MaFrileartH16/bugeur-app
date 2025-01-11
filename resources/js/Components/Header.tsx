d531a04bcaaf1afc53cf5b1922e1944fdcf62924
MaFrileartH16 <muhammadfauzulhanif16@gmail.com>
2025-01-11 19:21:51 +0700

    feat: add header component

---
+++ resources/js/Components/Header.tsx
@@ -0,0 +1,15 @@
+import { NavigationDrawer } from '@/Components/NavigationDrawer';
+import { UserMenu } from '@/Components/UserMenu';
+import { Container, Flex } from '@mantine/core';
+
+export const Header = () => {
+  return (
+    <Container py={16} bg="red" size="lg" w="100%">
+      <Flex justify="space-between">
+        <NavigationDrawer />
+
+        <UserMenu />
+      </Flex>
+    </Container>
+  );
+};
