5c1f066ddf347cc41d0b62cd6c72c44cdb508c2d
MaFrileartH16 <muhammadfauzulhanif16@gmail.com>
2025-01-11 17:14:19 +0700

    refactor(factory): optimize bug assignment in ScreenshotFactory

---
+++ database/factories/ScreenshotFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Bug;
+use App\Models\Screenshot;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Screenshot>
+ */
+class ScreenshotFactory extends Factory
+{
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      'bug_id' => Bug::inRandomOrder()->value('id') ?? Bug::factory()->create()->id,
+      'path' => $this->faker->imageUrl(),
+    ];
+  }
+}
