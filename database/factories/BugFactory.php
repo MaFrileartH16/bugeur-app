c866ca01f43b3f02abf98d8e03aa52708a14ec37
MaFrileartH16 <muhammadfauzulhanif16@gmail.com>
2025-01-11 17:11:56 +0700

    refactor(factory): optimize user assignment in BugFactory
    - Modified  to use existing users if available, otherwise create a new developer user
    - Modified  to use existing users if available, otherwise create a new tester user
    - Improved efficiency by reducing unnecessary user creation

--- database/factories/BugFactory.php
+++ database/factories/BugFactory.php
@@ -24,10 +24,10 @@ class BugFactory extends Factory
     return [
       'project_id' => Project::inRandomOrder()->value('id'),
       'title' => $this->faker->sentence(1),
-      'assignee_id' => User::factory()->create(['user_type' => 'developer'])->id,
+      'assignee_id' => User::inRandomOrder()->value('id') ?? User::factory()->create(['user_type' => 'developer'])->id,
       'status' => $this->faker->randomElement(['open', 'in_progress', 'resolved', 'closed']),
       'description' => $this->faker->paragraph,
-      'creator_id' => User::factory()->create(['user_type' => 'tester'])->id,
+      'creator_id' => User::inRandomOrder()->value('id') ?? User::factory()->create(['user_type' => 'tester'])->id,
       'deadline' => $this->faker->dateTimeBetween('now', '+1 month'),
       'bug_type' => $this->faker->randomElement(['critical', 'major', 'minor']),
     ];
