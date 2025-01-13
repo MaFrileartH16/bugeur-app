90defbc8d78afc949853e8f8693106b8dd7a5dc6
MaFrileartH16 <muhammadfauzulhanif16@gmail.com>
2025-01-13 11:36:55 +0700

    update

--- public/build/assets/Dashboard-BwNXSzb3.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./app-CwXZ0I_k.js";import{A as r}from"./AuthenticatedLayout-75b7ClUc.js";import"./AppLayout-DcDpKpJS.js";const i=t=>a.jsx(r,{title:t.title,children:"adsasdasd"});export{i as default};
---
+++ public/build/assets/Dashboard-DPjz8sda.js
@@ -0,0 +1,1 @@
+import{j as a}from"./app-BFxEyWYY.js";import{A as r}from"./AuthenticatedLayout-DBPGHiMl.js";import"./createReactComponent-B1rNj-2n.js";const i=t=>a.jsx(r,{title:t.title,children:"adsasdasd"});export{i as default};
