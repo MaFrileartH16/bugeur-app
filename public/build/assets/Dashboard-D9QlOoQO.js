869409c7673808c7e20117dac0bfad6042d6f980
MaFrileartH16 <muhammadfauzulhanif16@gmail.com>
2025-01-14 18:47:38 +0700

    update

---
+++ public/build/assets/Dashboard-D9QlOoQO.js
@@ -0,0 +1,1 @@
+import{j as i}from"./app-CrImoUcy.js";import{A as e,C as a,T as n}from"./AppLayout-Daupf-4s.js";const o=t=>i.jsx(e,{title:t.title,user:t.auth.user,notification:t.notification,children:i.jsx(a,{flex:1,size:"xl",children:i.jsx(n,{mb:32,children:t.title})})});export{o as default};
--- public/build/assets/Dashboard-DZ6hHQDd.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./app-qtcasp3t.js";import{A as e,C as a,T as n}from"./AppLayout-iONTU9Cg.js";const o=t=>i.jsx(e,{title:t.title,user:t.auth.user,notification:t.notification,children:i.jsx(a,{flex:1,size:"xl",children:i.jsx(n,{mb:32,children:t.title})})});export{o as default};
